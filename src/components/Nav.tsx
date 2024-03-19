1b897eb9ba64404f6044d4db4c6efad4a0cad5f5
Adam Janicki <adamjanicki2@gmail.com>
2024-03-19 14:08:49 -0400

    get rid of fixed nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -22,38 +22,35 @@ const Nav = () => {
   );
 
   return (
-    <>
-      <nav
-        className={`flex items-center justify-between w-100 bg-yellow nav pv2 ph4`}
-        id="nav"
-      >
-        <div className="flex items-center justify-between bar-container">
-          <Link className="nav-title flex items-center" to="/">
-            <Logo height="40px" width="40px" />
-            <span className="desktop ml2">QR Scanner</span>
-          </Link>
-          <div className="mobile">
-            <Hamburger
-              toggled={open}
-              onToggle={() => setOpen(!open)}
-              direction="left"
-              size={24}
-              duration={0.3}
-            />
-          </div>
+    <nav
+      className={`flex items-center justify-between w-100 bg-yellow nav pv2 ph4`}
+      id="nav"
+    >
+      <div className="flex items-center justify-between bar-container">
+        <Link className="nav-title flex items-center" to="/">
+          <Logo height="40px" width="40px" />
+          <span className="desktop ml2">QR Scanner</span>
+        </Link>
+        <div className="mobile">
+          <Hamburger
+            toggled={open}
+            onToggle={() => setOpen(!open)}
+            direction="left"
+            size={24}
+            duration={0.3}
+          />
         </div>
-        <ul
-          className={`flex items-center desktop link-container`}
-          style={{ display: open ? "flex" : undefined }}
-        >
-          <Navlink to="/">Home</Navlink>
-          <Navlink to="/scan">Scan</Navlink>
-          <Navlink to="/generate">Generate</Navlink>
-          <Navlink to="/lego">Lego</Navlink>
-        </ul>
-      </nav>
-      <div className="nav-padding" />
-    </>
+      </div>
+      <ul
+        className={`flex items-center desktop link-container`}
+        style={{ display: open ? "flex" : undefined }}
+      >
+        <Navlink to="/">Home</Navlink>
+        <Navlink to="/scan">Scan</Navlink>
+        <Navlink to="/generate">Generate</Navlink>
+        <Navlink to="/lego">Lego</Navlink>
+      </ul>
+    </nav>
   );
 };
 
