b9d68d1345626aa4172b80741b79809ec914846f
Adam Janicki <adamjanicki2@gmail.com>
2024-03-21 16:48:21 -0400

    maintenance mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,18 @@ import "tachyons/css/tachyons.min.css";
 import "src/css/style.css";
 import "src/css/media.css";
 import App from "src/App";
+import Maintenance from "src/pages/Maintenance";
+
+// Set to true to enable maintenance mode in production
+const MAINTENANCE_MODE = false;
+const mainComponent =
+  MAINTENANCE_MODE && process.env.NODE_ENV === "production" ? (
+    <Maintenance />
+  ) : (
+    <App />
+  );
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>{mainComponent}</React.StrictMode>);
---
+++ src/pages/Maintenance.tsx
@@ -0,0 +1,11 @@
+const Maintenance = () => (
+  <div
+    className="flex flex-column justify-center items-center"
+    style={{ width: "100vw", height: "100vh" }}
+  >
+    <h1 className="mv0">🚧 Under Maintenance 🚧</h1>
+    <p>Check back soon!</p>
+  </div>
+);
+
+export default Maintenance;
