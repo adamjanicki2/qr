dc05649cd3f4ff531cfb58983ddc4a61d5491056
Adam Janicki <adamjanicki2@gmail.com>
2024-03-22 19:31:31 -0400

    ace brickman

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,26 @@
 import { useDocumentTitle } from "src/hooks";
-import QRCode from "react-qr-code";
+import { Link } from "react-router-dom";
+import ace from "src/img/ace.webp";
 
 const Home = () => {
   useDocumentTitle("QR Scanner");
   return (
     <div className="flex flex-wrap home justify-center items-center">
-      <div className="mh3 flex flex-column items-center">
+      <div className="mh3 flex flex-column items-center ph3">
         <h1 className="home-title tc mv0 tc">QR Scanner</h1>
         <p className="home-description tc">
           Welcome to QR Scanner, where you can scan and generate QRs!
         </p>
+        <div className="flex justify-around items-center w-100 mv2 home-description">
+          <Link className="default-link i" to="/lego">
+            Scan Lego CMFs
+          </Link>
+          <Link className="default-link i" to="/scan">
+            Scan QRs
+          </Link>
+        </div>
       </div>
-      <QRCode
-        className="mh3"
-        value="https://adamjanicki.xyz/qr-scanner"
-        size={256}
-      />
+      <img src={ace} alt="" style={{ maxHeight: 300 }} className="ph3" />
     </div>
   );
 };
--- src/pages/Maintenance.tsx
+++ src/pages/Maintenance.tsx
@@ -3,8 +3,8 @@ const Maintenance = () => (
     className="flex flex-column justify-center items-center"
     style={{ width: "100vw", height: "100vh" }}
   >
-    <h1 className="mv0">🚧 Under Maintenance 🚧</h1>
-    <p>Check back soon!</p>
+    <h1 className="mv0 maintenance-title">🚧 Under Maintenance 🚧</h1>
+    <p className="f5 fw5">Check back soon!</p>
   </div>
 );
 
